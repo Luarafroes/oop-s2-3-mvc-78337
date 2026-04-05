1d588c85449a9a7fe4b74b9dfa3b079d57fdd6a1
Luarafroes <luara0511f@gmail.com>
2026-04-05 12:53:51 +0100

    add seq

--- VgcCollege.Web/ViewModels/StudentWithAttendanceViewModel.cs
+++ VgcCollege.Web/ViewModels/StudentWithAttendanceViewModel.cs
@@ -4,7 +4,7 @@ namespace VgcCollege.Web.ViewModels
 {
     public class StudentWithAttendanceViewModel
     {
-        public StudentProfile Student { get; set; }
+        public StudentProfile? Student { get; set; }
         public int AttendancePercent { get; set; }
         public int EnrolmentId { get; set; }
     }
